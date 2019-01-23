231b9572d888fc92ad78e9606d7e7e241016c33b
zalan <zlaval@gmail.com>
2019-01-23 19:29:12 +0100

    set colors

--- app/src/main/java/com/zlrx/tasktimer/MainActivity.kt
+++ app/src/main/java/com/zlrx/tasktimer/MainActivity.kt
@@ -1,15 +1,11 @@
 package com.zlrx.tasktimer
 
 import android.os.Bundle
-import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.google.android.material.snackbar.Snackbar
-import com.zlrx.tasktimer.model.Task
 import com.zlrx.tasktimer.viewmodel.TaskViewModel
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -22,7 +18,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
         taskViewModel = ViewModelProviders.of(this).get(TaskViewModel::class.java)
-        observeTasks()
+        // observeTasks()
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
@@ -30,18 +26,18 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun observeTasks() {
-        taskViewModel.tasks.observe(this, Observer {
-            it?.forEach { task ->
-                Log.i("TASK", task.toString())
-            }
-        })
-    }
-
-    fun insertTask(view: View) {
-        val task = Task(name = "Task")
-        taskViewModel.insert(task)
-    }
+//    private fun observeTasks() {
+//        taskViewModel.tasks.observe(this, Observer {
+//            it?.forEach { task ->
+//                Log.i("TASK", task.toString())
+//            }
+//        })
+//    }
+//
+//    fun insertTask(view: View) {
+//        val task = Task(name = "Task")
+//        taskViewModel.insert(task)
+//    }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -54,7 +50,7 @@ class MainActivity : AppCompatActivity() {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
-            R.id.action_settings -> true
+            R.id.mainMenuSettings -> true
             else -> super.onOptionsItemSelected(item)
         }
     }
